92765dbdbe3ec2d3cf2f21fb26d21ea86ae83420
Adeel Bashir <99957815+AdeelGameDev@users.noreply.github.com>
2026-06-02 12:47:53 +0500

    add performance manager

---
+++ Runtime/PerformanceManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PerformanceManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        // Disable VSync to allow manual frame rate control
+        QualitySettings.vSyncCount = 0;
+
+        // Lock the game to 60 FPS
+        Application.targetFrameRate = 60;
+
+        // Prevent the device from going to sleep
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+    }
+}
